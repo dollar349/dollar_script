781fe63458ade992b1acb2f194dd988d957214a6
dollar <dollar.wang@emerson.com>
2015-09-21 19:51:29 +0800

    Add MvToTrash.sh

---
+++ MvToTrash.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+T_NAME=/home/${USER}/trash/
+LOG_NAME=${T_NAME}/`date -u +%Y%m%d-%H%M%S`-$RANDOM
+mkdir -p ${T_NAME}
+echo "Move $* to ${LOG_NAME}"
+echo "And then delete them (y/n)? "
+read -p "y:commit, n: cancel : " ans
+
+if [ "$ans" == "y" ]; then
+  mkdir -p ${LOG_NAME}
+        eval mv $* ${LOG_NAME}
+        sudo -b nice --adjustment=19 rm -rf ${LOG_NAME}
+        echo "Deleting ${LOG_NAME} at background"
+else
+        echo "  CANCELED"
+fi
+
+
+
