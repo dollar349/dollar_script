b7a6544b76fe3db5c215ea84006b20b3982e4039
“dollar” <dollar.wang@emerson.com>
2015-09-21 19:56:43 +0800

    add git_init.sh

---
+++ git_init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git config --global user.name “dollar”
+git config --global user.email "dollar.wang@emerson.com"
+git config --global color.ui true
+git config --global alias.st status
+git config --global core.editor "vim"
