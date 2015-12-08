f2dd653d15120c65d4ec78a3041414722700701f
dollar <dollar.wang@emerson.com>
2015-12-08 11:17:33 +0800

    update update_oe-mirrror_download.sh

--- update_oe-mirrror_download.sh
+++ update_oe-mirrror_download.sh
@@ -6,7 +6,7 @@ if [[ $1 != "" ]]; then
 mkdir -p ${MOUNT_FOLDER}
 mkdir -p ${NEWFILE_FOLDER}
 
-sudo mount ${MOUNT_FOLDER} ${MIRRO_SERVER} && \ 
+sudo mount  -t nfs ${MOUNT_FOLDER} ${MIRRO_SERVER} && \ 
 diff --exclude="*.done" $1 ${MOUNT_FOLDER} | grep "Only in downloads"| awk '{print $4}' | grep -v "^etc$\|^git2$\|^share$\|^licenses$\|^test$\|bash43-*" | xargs -i cp -raf $1/{} ${NEWFILE_FOLDER}/.  
 sudo umount ${MOUNT_FOLDER} && rm -rf ${MOUNT_FOLDER}
 
