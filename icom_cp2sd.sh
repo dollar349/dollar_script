fd2056ef925742dcb46558cb9b0bcffcb78a881c
dollar <dollar.wang@emerson.com>
2016-02-02 15:40:02 +0800

    update icom_cp2sd.sh

--- icom_cp2sd.sh
+++ icom_cp2sd.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 if [[ $1 != "" ]]; then
-echo "copy to /media/rfs1"
+read -p "clean & update to /media/rfs1? ( y / n ):" ans
+
+if [ "$ans" == "y" ]; then
 sudo rm -rf /media/rfs1/* && \
 sudo tar -xvzf $1/icomcms-image-tm-am335x-cpm3.tar.gz -C /media/rfs1/ && \
 sudo tar -xvzf $1/modules-tm-am335x-cpm3.tgz -C /media/rfs1/
+fi
 
-echo "copy to /media/rfs2"
+read -p "clean & update to /media/rfs2? ( y / n ):" ans
+if [ "$ans" == "y" ]; then
 sudo rm -rf /media/rfs2/* && \
 sudo tar -xvzf $1/icomcms-image-tm-am335x-cpm3.tar.gz -C /media/rfs2/ && \
 sudo tar -xvzf $1/modules-tm-am335x-cpm3.tgz -C /media/rfs2/
-#sudo cp -raf $1/zImage /media/rfs1/boot/.
-#sudo cp -raf $1/am335x-ghmi.dtb /media/rfs1/boot/.
+fi
+
+read -p "clean & update to /media/boot? ( y / n ):" ans
+if [ "$ans" == "y" ]; then
+sudo rm -rf /media/boot/* && \
+sudo cp $1/u-boot.img /media/boot/. && \
+sudo cp $1/MLO /media/boot/.
+fi
+
+
 sudo sync
 else
 echo No image path enter !!!
