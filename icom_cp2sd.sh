f163b5fc88842b5a524d0236b3d94f3f0a9e10a8
dollar <dollar.wang@emerson.com>
2016-02-02 15:02:11 +0800

    update icom_cp2sd.sh

--- icom_cp2sd.sh
+++ icom_cp2sd.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 if [[ $1 != "" ]]; then
-sudo rm -rf /media/rfs1/*
-sudo tar -xvzf $1/icomcms-image-tm-am335x-cpm3.tar.gz -C /media/rfs1/
+echo "copy to /media/rfs1"
+sudo rm -rf /media/rfs1/* && \
+sudo tar -xvzf $1/icomcms-image-tm-am335x-cpm3.tar.gz -C /media/rfs1/ && \
 sudo tar -xvzf $1/modules-tm-am335x-cpm3.tgz -C /media/rfs1/
+
+echo "copy to /media/rfs2"
+sudo rm -rf /media/rfs2/* && \
+sudo tar -xvzf $1/icomcms-image-tm-am335x-cpm3.tar.gz -C /media/rfs2/ && \
+sudo tar -xvzf $1/modules-tm-am335x-cpm3.tgz -C /media/rfs2/
 #sudo cp -raf $1/zImage /media/rfs1/boot/.
 #sudo cp -raf $1/am335x-ghmi.dtb /media/rfs1/boot/.
 sudo sync
