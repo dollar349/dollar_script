19fc8f1096063fdb3b83215e8b8dade938c78568
“dollar” <dollar.wang@emerson.com>
2017-06-12 10:59:52 +0800

    update git_init.sh

--- git_init.sh
+++ git_init.sh
@@ -5,3 +5,7 @@ git config --global color.ui true
 git config --global alias.st status
 git config --global core.editor "vim"
 git config --global credential.helper store
+git config --global push.default current
+git config --global alias.ck checkout
+git config --global alias.br branch
+git config --global alias.cm commit
