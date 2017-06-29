ce3cb54c6ebb33b812047eb5c15aa743667cf1c4
“dollar” <dollar.wang@emerson.com>
2017-06-29 16:10:40 +0800

    Update e-mail

--- git_init.sh
+++ git_init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-git config --global user.name “dollar”
-git config --global user.email "dollar.wang@emerson.com"
+git config --global user.name “Dollar”
+git config --global user.email "Dollar.Wang@vertivco.com"
 git config --global color.ui true
 git config --global alias.st status
 git config --global core.editor "vim"
