d9aa602822fa022f1e2cbb6843922f2eccccbd4f
“Dollar” <Dollar.Wang@vertivco.com>
2017-09-30 10:15:43 +0800

    Update bashrc_add

--- GetReleaseFile_ForRPC2.sh
+++ GetReleaseFile_ForRPC2.sh
@@ -6,6 +6,7 @@ FOLDER_NAME=`basename "${FOLDER_PATH}"`
 RELEASE_FOLDER=`date +%Y_%m_%d`_ReleaseFor_${FOLDER_NAME}
 mkdir -p ${RELEASE_FOLDER} 
 cp -raf ${FOLDER_PATH}/rpc2k/build-rpc2k/tmp/deploy/images/RPC2_*_AppFwUpdt_DB.bin  ${RELEASE_FOLDER}/.
+cp -raf ${FOLDER_PATH}/rpc2k/build-rpc2k/tmp/deploy/images/RPC2_*_BootFw_DB.bin  ${RELEASE_FOLDER}/.
 cp -raf ${FOLDER_PATH}/sources/enp-velocity/core/Tools/Bin/e2_gdd.asn  ${RELEASE_FOLDER}/.
 cp -raf ${FOLDER_PATH}/sources/enp-velocity/core/Tools/Bin/e2_gdd.en  ${RELEASE_FOLDER}/.
 cp -raf ${FOLDER_PATH}/sources/enp-velocity/enpVsaLib/RPC2K.xml  ${RELEASE_FOLDER}/.
