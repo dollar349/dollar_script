313ad3b6da28619f0236797c056b6fee8fdd1460
“Dollar” <Dollar.Wang@vertivco.com>
2017-12-07 12:06:16 +0800

    Remove _DB for GetReleaseFile_ForRPC2.sh

--- GetReleaseFile_ForRPC2.sh
+++ GetReleaseFile_ForRPC2.sh
@@ -5,8 +5,8 @@ eval FOLDER_PATH=${FOLDER_PATH}
 FOLDER_NAME=`basename "${FOLDER_PATH}"`
 RELEASE_FOLDER=`date +%Y_%m_%d`_ReleaseFor_${FOLDER_NAME}
 mkdir -p ${RELEASE_FOLDER} 
-cp -raf ${FOLDER_PATH}/rpc2k/build-rpc2k/tmp/deploy/images/RPC2_*_AppFwUpdt_DB.bin  ${RELEASE_FOLDER}/.
-cp -raf ${FOLDER_PATH}/rpc2k/build-rpc2k/tmp/deploy/images/RPC2_*_BootFw_DB.bin  ${RELEASE_FOLDER}/.
+cp -raf ${FOLDER_PATH}/rpc2k/build-rpc2k/tmp/deploy/images/RPC2_*_AppFwUpdt.bin  ${RELEASE_FOLDER}/.
+cp -raf ${FOLDER_PATH}/rpc2k/build-rpc2k/tmp/deploy/images/RPC2_*_BootFw.bin  ${RELEASE_FOLDER}/.
 cp -raf ${FOLDER_PATH}/sources/enp-velocity/core/Tools/Bin/e2_gdd.asn  ${RELEASE_FOLDER}/.
 cp -raf ${FOLDER_PATH}/sources/enp-velocity/core/Tools/Bin/e2_gdd.en  ${RELEASE_FOLDER}/.
 cp -raf ${FOLDER_PATH}/sources/enp-velocity/enpVsaLib/RPC2K.xml  ${RELEASE_FOLDER}/.
