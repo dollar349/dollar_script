a62419b2bbe3e0299faac39518a4f3582413aed5
“Dollar” <Dollar.Wang@vertivco.com>
2018-07-05 14:37:17 +0800

    add file & update update_oe-mirror_download.sh

--- update_oe-mirrror_download.sh
+++ update_oe-mirrror_download.sh
@@ -7,9 +7,10 @@ mkdir -p ${MOUNT_FOLDER}
 mkdir -p ${NEWFILE_FOLDER}
 sudo mount -t nfs ${MIRRO_SERVER} ${MOUNT_FOLDER} && \
 diff --exclude="*.done" $1 ${MOUNT_FOLDER} | grep "Only in $1" | awk '{print $4}' | \
-grep -v "^etc$\|^git2$\|^share$\|^licenses$\|^test$\|bash43-*" | \
+grep -v "^etc$\|^git2$\|^share$\|^licenses$\|^test$\|^bash43-*\|^bash44-*" | \
 xargs -i cp -raf $1/{} ${NEWFILE_FOLDER}/.  
 sudo umount ${MOUNT_FOLDER} && rm -rf ${MOUNT_FOLDER}
+cd ${NEWFILE_FOLDER} && rm -rf *Avocent* && rm -rf *vertivco* && rm -rf edm* && rm -rf Stingray*
 echo "done"
 echo "if check ${NEWFILE_FOLDER} ok, issue command:"
 echo "scp ${NEWFILE_FOLDER}/* dollar@10.162.243.192:/sys_home/space_for_http_server/."
