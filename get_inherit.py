457d98aa0fe6a9a15037d4ee51b0cff6b0d98bc0
“Dollar” <Dollar.Wang@vertivco.com>
2020-06-07 12:52:23 +0800

    update get_inherit.py

--- get_inherit.py
+++ get_inherit.py
@@ -4,17 +4,25 @@ from pathlib import Path
 import sys
 
 cnt=len(sys.argv)
+Fileanme_list=[]
 
 if cnt != 2:
     exit(-1)
 
 for path in Path(os.getcwd()).rglob(sys.argv[1]):
-    print(path.absolute())
+    Fileanme_list.append(path.absolute())
+
+for Fileanme in Fileanme_list:
+        print (Fileanme)
+        fp = open(Fileanme, "r")
+        for line in iter(fp):
+            print (line)
+        fp.close()
 
 """
 fp = open('filename.txt', "r")
 for line in iter(fp):
-    print line
+    print (line)
 fp.close()
 """
 
