712d5142d8fc32a40f5e7495eacc7be4e177b48a
“Dollar” <dollar.wang@vertivco.com>
2020-08-05 10:05:47 +0800

    Update update_oe-mirrror_download.sh

--- update_oe-mirrror_download.sh
+++ update_oe-mirrror_download.sh
@@ -2,35 +2,35 @@
 #MIRRO_SERVER="10.162.243.220:/var/www/html/oe-mirror/cobra-xilinx-obmc2.7.0"
 SERVER_LIST=("10.162.243.220:/var/www/html/oe-mirror/xilinx-yocto" \
               "10.162.243.220:/var/www/html/oe-mirror/cobra-xilinx-obmc2.7.0" \
+              "10.162.243.220:/var/www/html/oe-mirror/cobra-xilinx-obmc2.7.0_yocto3.1"
               )
 MOUNT_FOLDER="tmp_mo"
 NEWFILE_FOLDER="new_files"
+if [[ -d $1 ]]; then
+    select opt in "${SERVER_LIST[@]}"
+    do
+        MIRRO_SERVER=$opt
+    
+        if [[ $opt == "" ]]; then
+        echo "bye bye!"
+        continue;
+        fi
+        break;
+    done
+    echo "MIRRO_SERVER = $MIRRO_SERVER"
 
-select opt in "${SERVER_LIST[@]}"
-do
-    MIRRO_SERVER=$opt
-  
-    if [[ $opt == "" ]]; then
-       echo "bye bye!"
-       continue;
-    fi
-    break;
-done
-echo "MIRRO_SERVER = $MIRRO_SERVER"
-
-if [[ $1 != "" ]]; then
-mkdir -p ${MOUNT_FOLDER}
-mkdir -p ${NEWFILE_FOLDER}
-sudo mount -t nfs ${MIRRO_SERVER} ${MOUNT_FOLDER} && \
-diff --exclude="*.done" $1 ${MOUNT_FOLDER} | grep "Only in $1" | awk '{print $4}' | \
-grep -v "^etc$\|^git2$\|^share$\|^licenses$\|^test$\|^bash43-*\|^bash44-*" | \
-xargs -i cp -raf $1/{} ${NEWFILE_FOLDER}/.  
-sudo umount ${MOUNT_FOLDER} && rm -rf ${MOUNT_FOLDER}
-cd ${NEWFILE_FOLDER} && rm -rf *Avocent* && rm -rf *vertivco* && rm -rf edm* && rm -rf Stingray*
-echo "done"
-echo "if check ${NEWFILE_FOLDER} ok, issue command:"
-echo "scp ${NEWFILE_FOLDER}/* dollar@${MIRRO_SERVER}/."
+    mkdir -p ${MOUNT_FOLDER}
+    mkdir -p ${NEWFILE_FOLDER}
+    sudo mount -t nfs ${MIRRO_SERVER} ${MOUNT_FOLDER} && \
+    diff --exclude="*.done" $1 ${MOUNT_FOLDER} | grep "Only in $1" | awk '{print $4}' | \
+    grep -v "^etc$\|^git2$\|^share$\|^licenses$\|^test$\|^bash43-*\|^bash44-*" | \
+    xargs -i cp -raf $1/{} ${NEWFILE_FOLDER}/.  
+    sudo umount ${MOUNT_FOLDER} && rm -rf ${MOUNT_FOLDER}
+    cd ${NEWFILE_FOLDER} && rm -rf *Avocent* && rm -rf *vertivco* && rm -rf edm* && rm -rf Stingray*
+    echo "done"
+    echo "if check ${NEWFILE_FOLDER} ok, issue command:"
+    echo "scp ${NEWFILE_FOLDER}/* dollar@${MIRRO_SERVER}/."
 else
-echo Please enter your downloads folder !!!
+    echo Please enter your downloads folder !!!
 fi
 
