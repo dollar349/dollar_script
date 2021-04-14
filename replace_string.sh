0c092bf1e916928a78b689d87ff4cc8e0e643a1a
“Dollar” <dollar.wang@vertivco.com>
2021-04-14 17:58:49 +0800

    Add replace_string.sh

---
+++ replace_string.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FIND_STRING=${1}
+NEW_STRING=${2}
+if test $# != 2; then
+   echo "invalid input"
+   exit 1 
+fi 
+grep -Hnrl "$1" . | xargs -i sed -i s/${FIND_STRING}/${NEW_STRING}/g {}
+exit 0
