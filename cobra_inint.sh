8c5c47c363bf86e4ffd4a320fdd3f788c4efe640
“Dollar” <Dollar.Wang@vertivco.com>
2021-11-30 10:01:03 +0800

    Update cobra_init

--- cobra_inint.sh
+++ cobra_inint.sh
@@ -3,6 +3,9 @@
 IP=${1}
 PWD="1"
 PORT="443"
+if test "x${2}" != "x"; then
+  PORT=${2}
+fi
 
 curl -ks -X PATCH -u "admin:Password1" "https://${IP}:${PORT}/redfish/v1/AccountService/Accounts/2" -H "content-type:application/json" -d "{\"Password\":\"${PWD}\"}" && \
 sleep 2 && \
