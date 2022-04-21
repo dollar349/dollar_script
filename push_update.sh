8122ab1b45896d7b5b4d35dea21a99c9f6484d68
“Dollar” <Dollar.Wang@vertivco.com>
2022-04-21 15:24:01 +0800

    Add Rest firmware update script

---
+++ push_update.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+RESOURCE_ID="fw_primary"
+PASSWD=""
+IP=""
+PORT=443
+image=""
+IP=""
+user="admin"
+FACTORY_RESET="N"
+print_help()
+{
+    echo ""
+    echo "This script helps to run push firmware update"
+    echo "  Usage: $(basename $0) -I \${target IP} -P \${PASSWORD} -i \${image(xxx.static.mtd.tar)} [options] ..."
+    echo "  option: "
+    echo "    -U [USER]"
+    echo "        admin as by default"
+    echo "    -A"
+    echo "        Update 2nd Flash"
+    echo "    -r"
+    echo "        reset to default"
+}
+while getopts 'I:P:i:U:rAh' OPT; do
+    case $OPT in
+        I)
+            IP=$OPTARG
+            ;;
+        P)
+            PASSWD=$OPTARG
+            ;;
+        i)
+            image=$OPTARG
+            ;;
+        U)
+            user=$OPTARG
+            ;;
+        A)
+            RESOURCE_ID="fw_alternative"
+            ;;
+        r)
+            FACTORY_RESET="Y"
+            ;;
+        h)
+            print_help
+            exit 0
+            ;;
+        ?)
+            print_help
+            exit 1
+            ;;
+    esac
+done
+
+if [ "${IP}" = "" ];then
+    echo "Please provide the target IP address by [-I \${target IP}]"
+    exit 1
+fi
+
+if [ "${PASSWD}" = "" ];then
+    echo "Please provide the Password for ${user} by [-P \${PASSWORD}]"
+    exit 1
+fi
+
+if [ "${image}" = "" ];then
+    echo "Please enter an image by [-i \${image(xxx.static.mtd.tar)}]"
+    exit 1
+fi
+
+
+#echo "curl -ks -u "${user}:${PASSWD}" HTTPS://${IP}:${PORT}/redfish/v1/UpdateService/FirmwareInventory/${RESOURCE_ID}"
+TARGET=`curl -ks -u "${user}:${PASSWD}" HTTPS://${IP}:${PORT}/redfish/v1/UpdateService/FirmwareInventory/${RESOURCE_ID} | jq '.RelatedItem[]."@odata.id"'`
+
+if [ "${TARGET}" = "" ];then
+    echo "Get update TARGET failed"
+    exit 1
+fi
+echo "update targe : ${TARGET}"
+
+# Set update target 
+curl -k -s -X PATCH -u "${user}:${PASSWD}" -H "Content-Type:application/json" -d "{\"HttpPushUriTargets\":[${TARGET}],\"HttpPushUriTargetsBusy\":true}" "https://${IP}:${PORT}/redfish/v1/UpdateService"
+sleep 1
+
+# Factory reset ?
+if [ ${FACTORY_RESET} = "Y" ];then
+    echo "Firmware update with reset to default..."
+    data='{"Oem": {"Vertiv": {"ResetToDefaults": true}}}'
+    curl -k -i -X PATCH -u ${user}:${PASSWD} -H "Content-Type:application/json" -d "$data" "HTTPS://${IP}:${PORT}/redfish/v1/UpdateService"
+fi
+
+# Start firmware update
+t1=$(date +"%s")
+TaskService=`curl -k -s -u ${user}:${PASSWD} -F "UpdateFile=@${image};filename=${image##*/}" https://${IP}:${PORT}/upload | jq .TaskMonitor`
+
+# trim double quotes
+TaskService=`echo ${TaskService} | tr -d '"'`
+
+echo "TaskService = ${TaskService}"
+re='^[0-9]+$'
+while
+    sleep 3
+    Progress=`curl -k -s -u ${user}:${PASSWD} https://${IP}:${PORT}${TaskService} | jq .PercentComplete`
+    echo "Progress = ${Progress}"
+    [[ ${Progress} =~ $re ]]
+do :;  done
+echo "Update completed..."
+t2=$(date +"%s")
+time=`expr $t2 - $t1`
+m=`expr $time / 60`
+s=`expr $time % 60`
+
+if [ $m -eq 0 ];then
+  echo "Time taken $s sec"
+else
+  echo "Time taken $m min $s sec"
+fi
+
+
---
+++ tftp_update.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+RESOURCE_ID="fw_primary"
+user="admin"
+FACTORY_RESET="N"
+PASSWD=""
+PORT=443
+tftp_image=""
+IP=""
+
+print_help()
+{
+    echo ""
+    echo "This script helps to run simple firmware update (TFTP)"
+    echo "  Usage: $(basename $0) -I \${target IP} -P \${PASSWORD} -i \${TFTP's image path} [options] ..."
+    echo "  option: "
+    echo "    -U [USER]"
+    echo "        admin as by default"
+    echo "    -A"
+    echo "        Update 2nd Flash"
+    echo "    -r"
+    echo "        reset to default"
+}
+while getopts 'I:P:i:U:rAh' OPT; do
+    case $OPT in
+        I)
+            IP=$OPTARG
+            ;;
+        P)
+            PASSWD=$OPTARG
+            ;;
+        i)
+            tftp_image=$OPTARG
+            ;;
+        U)
+            user=$OPTARG
+            ;;
+        A)
+            RESOURCE_ID="fw_alternative"
+            ;;
+        r)
+            FACTORY_RESET="Y"
+            ;;
+        h)
+            print_help
+            exit 0
+            ;;
+        ?)
+            print_help
+            exit 1
+            ;;
+    esac
+done
+
+if [ "${IP}" = "" ];then
+    echo "Please provide the target IP address by [-I \${target IP}]"
+    exit 1
+fi
+
+if [ "${PASSWD}" = "" ];then
+    echo "Please provide the Password for ${user} by [-P \${PASSWORD}]"
+    exit 1
+fi
+
+if [ "${tftp_image}" = "" ];then
+    echo "Please enter a TFTP's image by [-i \${TFTP's image path}]"
+    exit 1
+fi
+
+#echo "curl -ks -u "${user}:${PASSWD}" HTTPS://${IP}:${PORT}/redfish/v1/UpdateService/FirmwareInventory/${RESOURCE_ID}"
+TARGET=`curl -ks -u "${user}:${PASSWD}" HTTPS://${IP}:${PORT}/redfish/v1/UpdateService/FirmwareInventory/${RESOURCE_ID} | jq '.RelatedItem[]."@odata.id"'`
+
+if [ "${TARGET}" = "" ];then
+    echo "Get update TARGET failed"
+    exit 1
+fi
+echo "update targe : ${TARGET}"
+
+
+# Factory reset ?
+if [ ${FACTORY_RESET} = "Y" ];then
+    echo "Firmware update with reset to default..."
+    data='{"Oem": {"Vertiv": {"ResetToDefaults": true}}}'
+    curl -k -i -X PATCH -u ${user}:${PASSWD} -H "Content-Type:application/json" -d "$data" "HTTPS://${IP}:${PORT}/redfish/v1/UpdateService"
+fi
+
+data="{\"ImageURI\":\"tftp://${tftp_image}\",\"Targets\": [${TARGET}],\"TransferProtocol\":\"TFTP\"}"
+echo ${data}
+
+# Start firmware update
+t1=$(date +"%s")
+#echo "curl -k -s -X POST -u "${user}:${PASSWD}" -H "Content-Type:application/json" -d "${data}" https://${IP}:${PORT}/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate"
+TaskService=`curl -k -s -X POST -u "${user}:${PASSWD}" -H "Content-Type:application/json" -d "${data}" https://${IP}:${PORT}/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate | jq .TaskMonitor`
+
+# trim double quotes
+TaskService=`echo ${TaskService} | tr -d '"'`
+
+echo "TaskService = ${TaskService}"
+re='^[0-9]+$'
+while
+    sleep 1
+    Progress=`curl -k -s -u ${user}:${PASSWD} https://${IP}:${PORT}${TaskService} | jq .PercentComplete`
+    echo "Progress = ${Progress}"
+    [[ ${Progress} =~ $re ]]
+do :;  done
+echo "Update completed..."
+t2=$(date +"%s")
+time=`expr $t2 - $t1`
+m=`expr $time / 60`
+s=`expr $time % 60`
+
+if [ $m -eq 0 ];then
+  echo "Time taken $s sec"
+else
+  echo "Time taken $m min $s sec"
+fi
+
