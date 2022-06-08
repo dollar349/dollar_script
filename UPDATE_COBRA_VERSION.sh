ba15a4f00fe061aec6cee9e8bea6f75badab6b58
“Dollar” <Dollar.Wang@vertivco.com>
2022-06-08 16:39:15 +0800

    update UPDATE_COBRA_VERSION.sh

--- UPDATE_COBRA_VERSION.sh
+++ UPDATE_COBRA_VERSION.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$1" = -[hH] ]];then
+  echo "This script will find all local.conf and set the new vrsion to \"COBRA_VERSION\""
+  echo "  Usage: $(basename $0) \${Version that you want}"
+fi
+
+
 if [[ $1 != "" ]]; then
    find . -name local.conf | xargs -i sed -i "s/COBRA_VERSION =.*/COBRA_VERSION = \"${1}\"/" {}
 else
