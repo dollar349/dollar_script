6a06e55202374a140e75c9e61588c5f98b3da8be
“Dollar” <Dollar.Wang@vertivco.com>
2022-07-21 17:12:57 +0800

    Update BITBAKE_OBMC_IMAGE_CPTFTP.sh

--- BITBAKE_OBMC_IMAGE_CPTFTP.sh
+++ BITBAKE_OBMC_IMAGE_CPTFTP.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
-
+ABSPATH=$(readlink -f "$BASH_SOURCE")
+SCRIPTPATH=$(dirname "$ABSPATH")
+# Check _GET_PROJECT_INFO.sh script exist
+if test ! -f ${SCRIPTPATH}/_GET_PROJECT_INFO.sh; then
+    echo "_GET_PROJECT_INFO.sh not found"
+    exit 1
+fi
+source ${SCRIPTPATH}/_GET_PROJECT_INFO.sh
+if test "${PRJ_INFO}" != "OK" ; then
+    echo "Project path not found"
+    exit 1
+fi
+CURRENT_PWD=`pwd`
+cd ${PRJ_BUILD_PATH}
 bitbake obmc-phosphor-image && rm -rf /tftpboot/${USER}/image-* && cp tmp/deploy/images/*/image-* /tftpboot/${USER}/.
+cd ${CURRENT_PWD}
