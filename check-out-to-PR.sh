702bc71bfeddca0fd017cc9b260bb10f2a615b10
“Dollar” <Dollar.Wang@vertivco.com>
2022-11-09 23:38:26 +0800

    Update check-out-to-PR.sh

--- check-out-to-PR.sh
+++ check-out-to-PR.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 if [ $# -eq 1 ];then
-   PRBRANCH="PR_${1}"
-   git fetch origin pull/${1}/head:${PRBRANCH} && git checkout ${PRBRANCH}
+   ISGITHUB=`git remote -v | grep origin | grep github`
+   if test "${ISGITHUB}" != ""; then
+      #echo "github"
+      PRBRANCH="PR_${1}"
+      git fetch origin pull/${1}/head:${PRBRANCH} && git checkout ${PRBRANCH}
+   elif test "`git remote -v | grep origin | grep gitlab`" != ""; then
+      #echo "gitlab"
+      PRBRANCH="MR_${1}"
+      git fetch origin merge-requests/${1}/head:${PRBRANCH} && git checkout ${PRBRANCH}      
+   fi
 else
    echo please enter a PR number.
 fi
