530424097314f848e8324409f26273861a4fad6e
“Dollar” <Dollar.Wang@vertivco.com>
2022-11-09 23:39:31 +0800

    Update check-out-to-PR.sh

--- check-out-to-PR.sh
+++ check-out-to-PR.sh
@@ -3,11 +3,11 @@
 if [ $# -eq 1 ];then
    ISGITHUB=`git remote -v | grep origin | grep github`
    if test "${ISGITHUB}" != ""; then
-      #echo "github"
+      #echo "Github"
       PRBRANCH="PR_${1}"
       git fetch origin pull/${1}/head:${PRBRANCH} && git checkout ${PRBRANCH}
    elif test "`git remote -v | grep origin | grep gitlab`" != ""; then
-      #echo "gitlab"
+      #echo "Gitlab"
       PRBRANCH="MR_${1}"
       git fetch origin merge-requests/${1}/head:${PRBRANCH} && git checkout ${PRBRANCH}      
    fi
