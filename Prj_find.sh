a3379d5bd00fe2f5e3f57178495cd3905fc170b9
“Dollar” <Dollar.Wang@vertivco.com>
2022-11-18 14:19:04 +0800

    Update Prj_find.sh Prj_grep.sh

--- Prj_find.sh
+++ Prj_find.sh
@@ -7,3 +7,4 @@ if test "${PRJ_INFO}" != "OK" ; then
     echo "Project path not found"
     exit 1
 fi
+find ${PRJ_PATH} -name "$1" -o -path "${PRJ_PATH}/build-*" -prune | grep -v "^${PRJ_PATH}/build-"
--- Prj_grep.sh
+++ Prj_grep.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-#find . -type f -name "*" | grep -v ".svn" | xargs grep -Hn "$1" --color
-grep -Hnr "$1" --color . 
-exit 0
+ABSPATH=$(readlink -f "$BASH_SOURCE")
+SCRIPTPATH=$(dirname "$ABSPATH")
+
+source ${SCRIPTPATH}/_GET_PROJECT_INFO.sh
+if test "${PRJ_INFO}" != "OK" ; then
+    echo "Project path not found"
+    exit 1
+fi
+find ${PRJ_PATH} -type f -name "*" -o -path "${PRJ_PATH}/build-*" -prune | grep -v "^${PRJ_PATH}/build-" | grep -v ".svn" | xargs grep -Hn "$1" --color
