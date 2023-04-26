cdfccbee1320b2dd2dd6c7dc6250e4b2e9fe3f5b
“Dollar” <Dollar.Wang@vertiv.com>
2023-04-26 10:28:06 +0800

    Create Prj_set_cache.sh & Prj_set_clear.sh

---
+++ Prj_set_clear.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+ABSPATH=$(readlink -f "$BASH_SOURCE")
+SCRIPTPATH=$(dirname "$ABSPATH")
+
+source ${SCRIPTPATH}/_GET_PROJECT_INFO.sh
+if test "${PRJ_INFO}" != "OK" ; then
+    echo "Project path not found"
+    exit 1
+fi
+# Check is cobra env
+if test "`grep COBRA_VERSION ${PRJ_LOCAL_CONF}`" != "";then
+    #echo "This is Cobra env"
+    CONF_FILE="${PRJ_PATH}/cobra-conf/common/cobra-common.conf" 
+elif test "`grep INC_VERSION ${PRJ_LOCAL_CONF}`" != "";then
+    #echo "This is INC env"
+    CONF_FILE="${PRJ_PATH}/inc-conf/common/inc-common.conf" 
+elif test "`grep ACI_VERSION ${PRJ_LOCAL_CONF}`" != "";then
+    #echo "This is ACI env"
+    CONF_FILE="${PRJ_PATH}/aci-conf/common/aci-common.conf" 
+fi
+
+sed -i '/^BB_SRCREV_POLICY/s|=.*|= "clear"|' inc-conf/common/inc-common.conf
