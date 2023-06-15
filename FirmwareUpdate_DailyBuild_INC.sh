ccadc7e24e7f04f7a9e256e1a4f543411bf3edac
“Dollar” <Dollar.Wang@vertiv.com>
2023-06-15 13:34:01 +0800

    Created FirmwareUpdate_DailyBuild_ACI.sh & FirmwareUpdate_DailyBuild_INC.sh

---
+++ FirmwareUpdate_DailyBuild_INC.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+ABSPATH=$(readlink -f "$BASH_SOURCE")
+SCRIPTPATH=$(dirname "$ABSPATH")
+
+SERVER_IP="10.162.243.217"
+PRJ_NAME="inc"
+MACHINE=""
+MACHINE_LIST=$(curl -s http://${SERVER_IP}/${PRJ_NAME}/dailybuild/ | grep -oE 'href="([^"]+/)?[^/]+/"' | awk -F\" '{print $2}' | sed 's/\///g')
+
+# Check tftp_update.sh script exist
+if test ! -f ${SCRIPTPATH}/tftp_update.sh; then
+    echo "tftp_update.sh not found"
+    exit 1
+fi
+
+print_help()
+{
+    echo ""
+    echo "This script helps to update daily-builds firmware to your target."
+    echo "  Usage: $(basename $0) -I \${target IP} -P \${PASSWORD} [options] ..."
+    echo "  Example: ./$(basename $0) -I 10.162.247.34 -P 1"
+    echo "  option: "
+    echo "    -M [Machine]"
+    echo "        Give this option will not pop up Machine select menu"
+    echo "    -U [USER]"
+    echo "        admin as by default"
+    echo "    -A"
+    echo "        Update 2nd Flash"
+    echo "    -r"
+    echo "        reset to default"
+    
+}
+
+while getopts 'M:I:P:U:rAho' OPT; do
+    case $OPT in
+        M)
+            MACHINE=$OPTARG
+            ;;
+        I)
+            IP=$OPTARG
+            ;;
+        P)
+            PASSWD=$OPTARG
+            ;;
+        U)
+            user=$OPTARG
+            ;;
+        A)
+            EXTRA_OPTION+="-A "
+            ;;
+        r)
+            EXTRA_OPTION+="-r "
+            ;;
+        o)  
+            EXTRA_OPTION+="-o "
+            ;;
+        h)
+            print_help
+            exit 0
+            ;;
+        ?)
+            print_help
+            exit 1
+            ;;
+    esac
+done
+
+if test "${IP}" = "" ;then
+    echo "Please provide the target IP address by [-I \${target IP}]"
+    exit 1
+fi
+
+if test "${PASSWD}" = "" ;then
+    echo "Please provide the Password for ${user} by [-P \${PASSWORD}]"
+    exit 1
+fi
+
+if test "${MACHINE}" = "" ;then
+    select opt in ${MACHINE_LIST}
+    do
+        MACHINE=${opt}
+        if test "${opt}" = "" ; then
+        echo "bye bye!"
+        exit 1
+        continue;
+        fi
+        break;
+    done
+fi
+
+if test "${MACHINE_LIST#*${MACHINE}}" = "${MACHINE_LIST}" ; then
+    echo "${MACHINE} not support!!"
+    exit 1
+fi
+
+TFTP_SERVER_IP=${SERVER_IP}
+TFTP_PATH="${PRJ_NAME}/dailybuild/${MACHINE}/today"
+
+# Get the correct date from today's image folder
+IMAGE_DATE=$(curl -s http://${SERVER_IP}/${TFTP_PATH}/ | grep -oE  'image_date_[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{4}' | sed 's/image_date_//' | head -1)
+if test "${IMAGE_DATE}" = "";then
+    echo "Get image date failed, so exit"
+    exit 1
+fi
+
+# Get the correct image name from today's image folder
+IMAGE_NAME=$(curl -s http://${SERVER_IP}/${TFTP_PATH}/ | grep -oE 'obmc-phosphor-image[^"<]+"' | sed 's/"//g')
+if test "${IMAGE_NAME}" = "";then
+    echo "Get image name failed, so exit"
+    exit 1
+fi
+
+echo "##########################################"
+echo "# Daily build's image info                "
+echo "#    Date is ${IMAGE_DATE}                "
+echo "#    Image name is ${IMAGE_NAME}          "
+echo "##########################################"
+echo "Do you agree? (y/n)"
+read ans
+if test "${ans}" = "y" -o "${ans}" = "Y";then
+    ${SCRIPTPATH}/tftp_update.sh -i ${TFTP_SERVER_IP}/${TFTP_PATH}/${IMAGE_NAME} -P ${PASSWD} -I ${IP} ${EXTRA_OPTION}
+else
+    exit 0
+fi
